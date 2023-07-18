0ebcd3ab6df23624b9a898e5e265903f0ae84212
unknown <abvorobey@gmail.com>
2023-07-18 22:21:47 +0300

    домашнее, исправленное

--- noteBook.py
+++ noteBook.py
@@ -2,12 +2,16 @@
 import datetime
 import json
 
+
+
 def add(note):
+
     id = 1
     head = input("\n""Введите заголовок заметки: ""\n")
     body = input("\n""Введите тело заметки: ""\n")
     time = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
     text = list()
+
     while True:
         if id in note:
             id += 1
@@ -19,52 +23,60 @@ def add(note):
             text.append(body)
             break
 
+
+
 def read(note):
+
     while True:
-        menuRead = input("\n""Какие заметки вывести?""\n"
+        menu_number = input("\n""Какие заметки вывести?""\n"
                         "1 - вывести весь список" "\n"
                         "2 - выбрать дату добавления/изменения заметки" "\n" )
-        if menuRead == "1":
+        
+        if menu_number == "1":
             for i in note:
                 print()
-                print(note[i][0])
-                print(note[i][1])
-                print(note[i][2])
-                print(note[i][3])
-                print()
+                for j in range(len(note[i])):
+                    print(note[i][j])
             break
-        if menuRead == "2":
-            dateElement = input("\n""Введите дату в формате ГГГГ-ММ-ДД:""\n")
+
+        if menu_number == "2":
+            date_element = input("\n""Введите дату в формате ГГГГ-ММ-ДД:""\n")
+
             for i in note:
-                if dateElement in note[i][1]:
+                if date_element in note[i][1]:
                     print()
-                    print(note[i][0])
-                    print(note[i][1])
-                    print(note[i][2])
-                    print(note[i][3])
-                    print()
+                    for j in range(len(note[i])):
+                        print(note[i][j])
+
                 else:
                     print("\n""По указанной дате нет заметок...")
                     break
-            break                   
-        print("\n""Такой команды нет! Еще раз: ")    
+            break    
+
+        print("\n""Такой команды нет! Еще раз: ") 
+
+
 
 def redact(note):
-    element = input("\n""Введите параметр для поиска: ""\n")
+
+    redact_element = input("\n""Введите параметр для поиска: ""\n")
     count = 0
+
     for i in note:    
         for j in range(1, len(note[i])):
-            if element in note[i][j]:
+            if redact_element in note[i][j]:
                 count += 1
                 print("\n""Найдена заметка:")
                 print(note[i])
+
                 while True:
-                    number = input("1 - редактируем заметку""\n"
-                                   "2 - оставляем как есть""\n")
-                    if number == "1" or number == "2":
+                    menu_number = input("1 - редактируем заметку""\n"
+                                   "2 - оставляем как есть""\n")                   
+                    if menu_number == "1" or menu_number == "2":
                         break
                     print("\n""Нет такой команды, выберите 1 или 2:")
-                if number == "1":
+
+                if menu_number == "1":
                     head = input("\n""Введите заголовок заметки: ""\n")
                     body = input("\n""Введите тело заметки: ""\n")
                     time = datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S")
@@ -75,72 +87,98 @@ def redact(note):
                     text.append(body)
                     note[i] = text
                     break
-                if number == "2":
+
+                if menu_number == "2":
                     pass
+
     if count == 0:
         print("Не нашлось ни одной записи...")
-    print(note)   
+
+    print(note) 
+
+
 
 def delete(note):
-    element = input("Введите параметр для поиска: ""\n")
+
+    delete_element = input("Введите параметр для поиска: ""\n")
     count = 0
-    listToDelete = list() 
+    list_to_delete = list() 
      
     for i in note:    
-        for j in range(1, len(note[i])):
-            if element in note[i][j]:
+        for j in range(1, len(note[i])):          
+            if delete_element in note[i][j]:
                 count += 1
                 print("Найдена заметка:")
                 print(note[i])
+
                 while True:
-                    number = input("1 - удаляем заметку""\n"
+                    menu_number = input("1 - удаляем заметку""\n"
                                    "2 - оставляем как есть""\n")
-                    if number == "1" or number == "2":
+                    if menu_number == "1" or menu_number == "2":
                         break
                     print("\n""Нет такой команды, выберите 1 или 2:""\n")
-                if number == "1":
-                    listToDelete.append(i)
+
+                if menu_number == "1":
+                    list_to_delete.append(i)
                     break
-                if number == "2":
+
+                if menu_number == "2":
                     pass
+
     if count == 0:
         print("Не нашлось ни одной записи...")
-    for i in listToDelete:
+
+    for i in list_to_delete:
         del note[i]
+
     print(note) 
 
-def askForExit(flag):
+
+
+def can_exit(flag):
+
     while True:
-            menuExit = input("\n""Продолжить работу с приложением: (y/n)?""\n")
-            if menuExit.lower() == "y":
+            exit_menu = input("\n""Продолжить работу с приложением: (y/n)?""\n")            
+            if exit_menu.lower() == "y":
                 return flag
-            if menuExit.lower() == "n":
+            
+            if exit_menu.lower() == "n":
                 flag = False
-                return flag           
+                return flag
+                       
             print("Такой команды нет! Еще раз: ")
 
-def parseNote(note):
-    noteValues = list()
-    noteKeys = list()
+
+
+def parse(note):
+
+    note_values = list()
+    note_keys = list()
+
     for i in note:
-        noteValues.append(note[i])
-        noteKeys.append(int(note[i][0]))
+        note_values.append(note[i])
+        note_keys.append(int(note[i][0]))
+
     note = {}
-    for i in range(len(noteKeys)):
-        note[noteKeys[i]] = noteValues[i]
+
+    for i in range(len(note_keys)):
+        note[note_keys[i]] = note_values[i]
+
     return note
 
 
 # Поехали!
 try:
-    with open('NoteBook.txt', 'r', encoding = 'utf-8') as jsonFile:
-        note = json.load(jsonFile)
+    with open('NoteBook.txt', 'r', encoding = 'utf-8') as json_file:
+        note = json.load(json_file)
 except:
-    with open('NoteBook.txt', 'w', encoding = 'utf-8') as jsonFile:
+    with open('NoteBook.txt', 'w', encoding = 'utf-8') as json_file:
         note = {}
-        json.dump(note, jsonFile)
-note = parseNote(note)
+        json.dump(note, json_file)
+
+note = parse(note)
 flag = True
+
 while flag:   
     print("\n""Введите номер команды:" "\n"
         "1 - создать заметку" "\n"
@@ -148,26 +186,33 @@ while flag:
         "3 - редактировать заметки" "\n"
         "4 - удалить заметки" "\n"
         "5 - выход из приложения" "\n")    
-    menuNumber = input()
-    if menuNumber == "1":
+    number_menu = input()
+
+    if number_menu == "1":
         add(note)
         print(note)
-        flag = askForExit(flag)        
-        continue       
-    if menuNumber == "2":
+        flag = can_exit(flag)        
+        continue
+
+    if number_menu == "2":
         read(note)
-        flag = askForExit(flag)
+        flag = can_exit(flag)
         continue
-    if menuNumber == "3":
+
+    if number_menu == "3":
         redact(note)
-        flag = askForExit(flag)
+        flag = can_exit(flag)
         continue
-    if menuNumber == "4":
+
+    if number_menu == "4":
         delete(note)
-        flag = askForExit(flag)
+        flag = can_exit(flag)
         continue
-    if menuNumber == "5":
+
+    if number_menu == "5":
         break
+
     print("Команда неверная, попробуйте еще раз: ")
-with open('NoteBook.txt', 'w', encoding = 'utf-8') as jsonFile:
-    json.dump(note, jsonFile)
+
+with open('NoteBook.txt', 'w', encoding = 'utf-8') as json_file:
+    json.dump(note, json_file)
